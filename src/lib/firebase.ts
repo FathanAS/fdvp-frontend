281d6349e3d44c8ae6af6752a03d47a094c2bf3c
FathanAS <cpmsire@gmail.com>
2026-01-04 23:00:19 +0700

    Hardcode Firebase Config for Debugging

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,6 +5,16 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"; // 1. Pastikan import ini ada
 
 const firebaseConfig = {
+  // HARDCODED CONFIG (DEBUG MODE)
+  apiKey: "AIzaSyAEKtVSzx_kgl7U8727yCtjelz9oEG3knI",
+  authDomain: "fdvp-db.firebaseapp.com",
+  projectId: "fdvp-db",
+  storageBucket: "fdvp-db.firebasestorage.app",
+  messagingSenderId: "1085243279530",
+  appId: "1:1085243279530:web:f6a32fe0abff4dae37e8d3",
+  measurementId: "G-NF8P5HKCCJ",
+
+  /*
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -12,6 +22,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  */
 };
 
 // Singleton pattern
